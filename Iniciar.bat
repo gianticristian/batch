25f1bff2d3ba211f158a1364c1fc2d40d9fc4186
Gianti Cristian <gianticristian@gmail.com>
2018-08-03 08:13:40 -0300

    Modificación de tiempos y orden

--- Iniciar.bat
+++ Iniciar.bat
@@ -1,7 +1,6 @@
 @echo off
 title Iniciar Programas
-set esperaCorta=15
-set esperaLarga=30
+
 
 
 :Inicio
@@ -27,58 +26,59 @@ goto Inicio
 set fichador=d:\"OneDrive - OCA\Instaladores\UNIX Sucursales\- Fichada.r2w"
 tasklist /nh /fi "Imagename eq r2win.exe" | find /i "r2win.exe" >nul && ( echo: ) || ( 
 start /max %fichador% 
-timeout /t %esperaLarga%
+timeout /t 30
 )
 
 ::AudioSwitcher
 set audioSwitcher=c:\"Program Files\Audio Switcher\AudioSwitcher.exe"
 tasklist /nh /fi "Imagename eq AudioSwitcher.exe" | find /i "AudioSwitcher.exe" >nul && ( echo: ) || ( 
 start %audioSwitcher% 
-timeout /t %esperaCorta%
+timeout /t 15
 )
 
 ::Shotty
 set shotty=c:\"Program Files\Shotty\Shotty.exe"
 tasklist /nh /fi "Imagename eq Shotty.exe" | find /i "Shotty.exe" >nul && ( echo: ) || ( 
 start %shotty% 
-timeout /t %esperaCorta%
-)
-
-::OneDrive
-set oneDrive=c:\Users\gianti.cristian\AppData\Local\Microsoft\OneDrive\OneDrive.exe
-tasklist /nh /fi "Imagename eq OneDrive.exe" | find /i "OneDrive.exe" >nul && ( echo: ) || ( 
-start %oneDrive% /background 
-timeout /t %esperaLarga%
+timeout /t 15
 )
 
 ::Outlook
 set outlook=c:\"Program Files (x86)\Microsoft Office\Office14\Outlook.exe"
 tasklist /nh /fi "Imagename eq Outlook.exe" | find /i "Outlook.exe" >nul && ( echo: ) || ( 
 start %outlook% 
-timeout /t %esperaLarga%
+timeout /t 60
 )
 
 ::Tickets
 set tickets=c:\"Program Files\Microsoft System Center 2012 R2\Service Manager\Microsoft.EnterpriseManagement.ServiceManager.UI.Console.exe"
 tasklist /nh /fi "Imagename eq Microsoft.EnterpriseManagement.ServiceManager.UI.Console.exe" | find /i "Microsoft.EnterpriseManagement.ServiceManager.UI.Console.exe" >nul && ( echo: ) || ( 
 start %tickets%
-timeout /t %esperaLarga%
+timeout /t 120
 )
 
 ::InternetExplorer
 set internetExplorer=c:\"Program Files (x86)\Internet Explorer\iexplore.exe"
 tasklist /nh /fi "Imagename eq iexplore.exe" | find /i "iexplore.exe" >nul && ( echo: ) || ( 
 start %internetExplorer% "oep.servicios.oca"
-timeout /t %esperaCorta%
+timeout /t 60
 )
 
 ::SQL
 set sql=c:\"Program Files (x86)\Microsoft SQL Server\110\Tools\Binn\ManagementStudio\Ssms.exe"
 tasklist /nh /fi "Imagename eq Ssms.exe" | find /i "Ssms.exe" >nul && ( echo: ) || ( 
 start %sql%
-timeout /t %esperaLarga%
+timeout /t 120
+)
+
+::OneDrive
+set oneDrive=c:\Users\gianti.cristian\AppData\Local\Microsoft\OneDrive\OneDrive.exe
+tasklist /nh /fi "Imagename eq OneDrive.exe" | find /i "OneDrive.exe" >nul && ( echo: ) || ( 
+start %oneDrive% /background 
+timeout /t 120
 goto Salir
 )
+
 
 :Salir
 exit
